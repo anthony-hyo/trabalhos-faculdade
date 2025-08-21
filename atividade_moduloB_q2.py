6fa2493eb519e77e11b42545ee42b8b9d9ba09df
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-08-21 00:08:04 -0300

    Melhoria no Docstrings e arrumado as exigências pedidas pelo professor

--- atividade_moduloB_q2.py
+++ atividade_moduloB_q2.py
@@ -6,27 +6,30 @@ class Style:
     GREEN = "\033[32m"
     BLUE = "\033[34m"
     BOLD = "\033[1m"
+    MAGENTA = "\033[35m"
     DANGER = "\033[97;41m"
     ALERT = "\033[30;43m"
 
 
-def beautiful_io(açao: Callable, mensagem: str, tipo: str = Style.GREEN, bold: bool = False, validar_acao: Callable = None, validar_mensagem: str = None) -> str | None:
-    """
+def beautiful_io(acao: Callable, mensagem: str, tipo: str = Style.GREEN, bold: bool = False, validar_acao: Callable = None, validar_mensagem: str = None) -> str | None:
+    """Entrada e Saída com validação e estilo
 
-    :param açao: Função input ou print
+    :param acao: Função input ou print
     :param mensagem: Mensagem exibida para o usuário
     :param tipo: Tipo da mensagem
-    :param bold:
+    :param bold: Retorna a mensagem com estilo bold
     :param validar_acao: Função de validação para entrada inválida
     :param validar_mensagem: Mensagem de validação para entrada inválida
 
-    :return: int | float | None
+    :return: str | None
     """
     while True:
         try:
-            valor = açao(f"{tipo}{Style.BOLD if bold else ''}{mensagem}{Style.RESET}")
+            valor = acao(f"{tipo}{Style.BOLD if bold else ''}{mensagem}{Style.RESET}")
 
-            if açao == input:
+            # Verifica se é do tipo input
+            if acao == input:
+                # Verifica se existe validação e executa
                 if validar_acao is not None and validar_acao(valor):
                     if validar_mensagem is None:
                         beautiful_io(print, "Inválido! Tente novamente.", Style.ALERT)
@@ -44,8 +47,6 @@ def beautiful_io(açao: Callable, mensagem: str, tipo: str = Style.GREEN, bold: 
             print(f"{Style.DANGER}Erro! Tente novamente.{Style.RESET}")
             print()
 
-
-beautiful_io(print, "Bem-vindo a Loja do Anthony De Sousa Fidelis!", bold=True)
 
 marmitas = {
     'BA': {
@@ -78,43 +79,78 @@ marmitas = {
     },
 }
 
+beautiful_io(print, "Bem-vindo a Loja do Anthony De Sousa Fidelis!", bold=True)
+
+print()
+beautiful_io(print, "Menu do Cliente:", Style.MAGENTA)
+beautiful_io(print, "Sabores disponíveis: BA (Bife Acebolado), FF (Filé de Frango)", Style.MAGENTA)
+beautiful_io(print, "Tamanhos disponíveis: P, M, G", Style.MAGENTA)
+print()
+
 pedidos = []
 
 while True:
+    # Solicita o sabor
     sabor = beautiful_io(
         input,
-        f"Entre com o sabor desejado (BA/FF): ",
-
+        "Entre com o sabor desejado (BA/FF): ",
         Style.BLUE,
-
         validar_acao=lambda s: s.upper() not in marmitas,
-        validar_mensagem="Sabor inválido! Tente novamente.",
+        validar_mensagem="Sabor inválido. Tente novamente",
     ).upper()
 
+    # Solicita o tamanho
     tamanho = beautiful_io(
         input,
         "Entre com o tamanho desejado (P/M/G): ",
-
         Style.BLUE,
-
         validar_acao=lambda t: t.upper() not in marmitas[sabor],
-        validar_mensagem="Tamanho inválido! Tente novamente.",
+        validar_mensagem="Tamanho inválido. Tente novamente",
     ).upper()
 
-    beautiful_io(
-        print,
-        f"Você pediu um {marmitas[sabor][tamanho]['Nome']} no tamanho {tamanho} por R$ {marmitas[sabor][tamanho]['Preço']}{Style.RESET}",
-        Style.GREEN
-    )
+    # TODO: Substituir por um dicionário ex: marmitas[sabor][tamanho]['Preço']
+    if sabor == "BA":
+        if tamanho == "P":
+            nome = "Bife Acebolado"
+            preco = marmitas["BA"]["P"]['Preço']
+        elif tamanho == "M":
+            nome = "Bife Acebolado"
+            preco = marmitas["BA"]["M"]['Preço']
+        else:
+            nome = "Bife Acebolado"
+            preco = marmitas["BA"]["G"]['Preço']
+    else:
+        if tamanho == "P":
+            nome = "Filé de Frango"
+            preco = marmitas["FF"]["P"]['Preço']
+        elif tamanho == "M":
+            nome = "Filé de Frango"
+            preco = marmitas["FF"]["M"]['Preço']
+        else:
+            nome = "Filé de Frango"
+            preco = marmitas["FF"]["G"]['Preço']
+
+    # Exibe o pedido
+    beautiful_io(print, f"Você pediu um {nome} no tamanho {tamanho} por R$ {preco}", Style.GREEN)
 
     print()
 
-    pedidos.append(marmitas[sabor][tamanho])
+    pedidos.append({
+        'sabor': sabor,
+        'preco': preco,
+        'Tamanho': tamanho
+    })
+
+    resposta = beautiful_io(input, "Deseja pedir mais alguma coisa? (S/N): ", Style.BLUE).upper()
 
     # Exibe cotinuar ou sair
-    if not input("Deseja realizar outro pedido? (S/N): ").strip().upper() == "S":
-        total = sum(pedido["Preço"] for pedido in pedidos)
+    match resposta:
+        case "S":
+            continue
+        case "N":
+            # Calcula total
+            total = sum(pedido["preco"] for pedido in pedidos)
 
-        beautiful_io(print, f"O valor total a ser pago: R$ {total:.2f}", Style.ALERT)
-
-        break
+            # Exibe o total a ser pago
+            beautiful_io(print, f"O valor total a ser pago: R$ {total:.2f}", Style.ALERT)
+            break
